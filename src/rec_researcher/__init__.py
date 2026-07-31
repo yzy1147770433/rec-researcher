df812c80a3f4adf41abcace0f9e04842a2f382ed
yzy1147770433 <yzy1147770433@gmail.com>
2026-07-31 10:20:07 +0800

    release: prepare rec researcher v0.2.0

--- src/rec_researcher/__init__.py
+++ src/rec_researcher/__init__.py
@@ -5,6 +5,6 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("rec-researcher")
 except PackageNotFoundError:  # pragma: no cover - source tree without installation
-    __version__ = "0.1.0"
+    __version__ = "0.2.0"
 
 __all__ = ["__version__"]
